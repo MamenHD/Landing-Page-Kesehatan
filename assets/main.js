8a83409c5f80476f1831cd15907c692393912c6a
Indra Nur Muhammad Ramadhan <mamenhd27@gmail.com>
2025-05-20 16:16:09 +0700

    Perbaikan responsive

--- assets/main.js
+++ assets/main.js
@@ -13,3 +13,37 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+// Scroll Animastion
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header_content form", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".header_image_card", {
+    duration: 1000,
+    interval: 500,
+    delay: 2500,
+});
